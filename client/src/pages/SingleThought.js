6cb49ac5ae6dd5d3ab1e036d0029b2ee1b1d0650
jambrose0 <jambrose0518@gmail.com>
2022-05-24 12:13:49 -0400

    functinoality is all wrapped up

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_THOUGHTS } from "../utils/queries";
+import { useParams } from "react-router-dom";
+import ReactionList from "../components/ReactionList";
 
-const SingleThought = props => {
+const SingleThought = (props) => {
+  const { id: thoughtId } = useParams();
+
+  const { loading, data } = useQuery(QUERY_THOUGHTS, {
+    variables: { id: thoughtId },
+  });
+
+  const thought = data?.thought || {};
+
+  if (loading) {
+    return <div>Loading....</div>;
+  }
+
+  console.log(thoughtId);
   return (
     <div>
       <div className="card mb-3">
         <p className="card-header">
           <span style={{ fontWeight: 700 }} className="text-light">
-            Username
-          </span>{' '}
-          thought on createdAt
+            {thought.username}
+          </span>{" "}
+          thought on {thought.createdAt}
         </p>
         <div className="card-body">
-          <p>Thought Text</p>
+          <p>{thought.thoughtText}</p>
         </div>
       </div>
+
+      {thought.reactionCount > 0 && (
+        <ReactionList reactions={thought.reactions} />
+      )}
     </div>
   );
 };
